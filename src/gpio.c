fad9dfffc37a3e6f782b6266bec8d50955cc6cfc
Andy Gallagher <andy.gallagher@theguardian.com>
2025-03-25 19:26:37 +0000

    tuning the timer and setting up strobe

--- src/gpio.c
+++ src/gpio.c
@@ -3,8 +3,8 @@
 void setup_gpio() {
     DDRC = 0x0F;    //lower 4 bits of PORTC are output
     DDRD = 0x1F;    //lower 5 bits of PORTD are output
-    PORTC = 0x0;
-    PORTD = 0xFF;
+    PORTC = 0x0F;   //initialise port C (cathodes) to 'high'
+    PORTD = 0x00;   //initialise port D (anodes) to 'low'
 
     //TBD - fix levels of unused pins as per manual
 }
--- src/timer.c
+++ src/timer.c
@@ -8,9 +8,11 @@
 void setup_timer() 
 {
     TCCR2A = 0x0;
-    TCCR2B = 1<<CS00;  //set CS00 => drive from clock, no prescaler  
+    TCCR2B = 1<<CS21;  //set CS20&CS21=> drive from clock, 1/8 prescaler. At 1/32 you can see the led flickering.
     TIMSK2 = 1<<TOIE0; //set TOIE0 => enable overflow interrupt
 }
+
+uint8_t cathode_counter = 0;
 
 //Interrupt handler for the timer. This should get called every 256 clock cycles.
 //Manual says a default 1Mhz clock; that makes this called about 4,000 times per second.
@@ -21,4 +23,9 @@ ISR(TIMER2_OVF_vect) {
     } else {
         PORTD |= 0x01;
     }
+
+    //strobe the cathodes
+    ++cathode_counter;
+    if(cathode_counter>=4) cathode_counter=0;
+    PORTC = ~(1<<cathode_counter);
 }
